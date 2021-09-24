0f47639042bf88af2dcd0a4594736409c3e67678
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2021-09-24 21:32:46 +0200

    Update stock_data.py

--- stock_data.py
+++ stock_data.py
@@ -1,5 +1,6 @@
 import pandas_datareader.data as web
 
+# Fetching data from Stooq API and returning current trend direction for given stock
 def get_data(stock):
     try:
         trend = {}
@@ -7,10 +8,13 @@ def get_data(stock):
         data = web.DataReader(stock, 'stooq')
         data = data['Close']
         data = data[::-1]
+        
+        # Creating variables for key technical lookback periods
         ma_50 = data.rolling(50).mean()
         ma_100 = data.rolling(100).mean()
         ma_200 = data.rolling(200).mean()
 
+        # Comparing actual results
         if data[-1] > ma_50[-1]:
             trend['Short_Term_Trend'] = 'Positive'
         else:
