5b470e62dfa2f8a0cd259c38a8ffbb8867db955d
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2024-07-14 18:29:31 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,40 +1,60 @@
+# Import TestClient from fastapi.testclient for testing the FastAPI app
 from fastapi.testclient import TestClient
+# Import the FastAPI app instance from the main module
 from main import app
+# Import the get_data function from the stock_data module
 from stock_data import get_data
 
-
+# Create a TestClient instance for testing the FastAPI app
 client = TestClient(app)
 
 
-# Ensures that the application instance exists
 def test_app_exists():
+    """
+    Test to ensure that the FastAPI application instance exists.
+    """
     assert app != None
 
-# Ensures that index page loads correctly
 def test_index_get():
+    """
+    Test to ensure that the index page loads correctly.
+    Verifies that the status code is 200 and the response contains the expected HTML content.
+    """
     response = client.get("/")
     assert response.status_code == 200
     assert '<title>Stooq Data API</title>' in response.text 
 
-# Ensures that stock page behaves as expected given valid symbol
 def test_stock_success():
+    """
+    Test to ensure that the stock page behaves as expected when given a valid stock symbol.
+    Verifies that the status code is 200 and the response JSON matches the expected trend data.
+    """
     response = client.get("/stock/amzn")
     assert response.status_code == 200
     assert response.json() == {"Short_Term_Trend": "Negative", "Medium_Term_Trend": "Negative", "Long_Term_Trend": "Positive"}
 
-# Ensures that stock page behaves as expected given invalid symbol
 def test_stock_failure():
+    """
+    Test to ensure that the stock page behaves as expected when given an invalid stock symbol.
+    Verifies that the status code is 404 and the response JSON contains the appropriate error message.
+    """
     response = client.get("/stock/wrong_symbol")
     assert response.status_code == 404
     assert response.json() == {'detail': 'Not Found'}
 
-# Ensures that get_data method works correctly given valid symbol
 def test_get_data_success():
+    """
+    Test to ensure that the get_data method works correctly when given a valid stock symbol.
+    Verifies that the returned trend data matches the expected result.
+    """
     data = get_data('amzn')
     result = {"Short_Term_Trend": "Negative", "Medium_Term_Trend": "Negative", "Long_Term_Trend": "Positive"}
     assert result == data
 
-# Ensures that get_data method works correctly given invalid symbol
 def test_get_data_failure():
+    """
+    Test to ensure that the get_data method works correctly when given an invalid stock symbol.
+    Verifies that the method returns None for an invalid symbol.
+    """
     data = get_data('wrong_symbol')
     assert data == None
